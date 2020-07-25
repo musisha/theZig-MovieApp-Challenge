f9660f7471a537f894d610cb025c9f6b0a3e400d
musisha <42687245+musisha@users.noreply.github.com>
2020-07-25 15:20:59 +0000

    Update config.ts

--- interview-coding-challenge-master/clientside/zig-movie-app/src/config.ts
+++ interview-coding-challenge-master/clientside/zig-movie-app/src/config.ts
@@ -1,9 +1,6 @@
 // Configuration for TMDB
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 
-const API_URL: string = 'https://api.themoviedb.org/3/';
-const API_KEY: string = 'efe61e21b11fc4b6ff49e29d28597602';
-
 // Images
 // An image URL looks like this example:
 // http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
